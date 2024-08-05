5c9f17879dd2ac6982ca0d1dc0fdf148bda2f2dd
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-05 18:59:46 +0700

    feat: Refactor Sorotan component to use SorotanCard for displaying highlighted content

--- src/app/components/sorotan.tsx
+++ src/app/components/sorotan.tsx
@@ -1,29 +1,58 @@
 import {GlareCard} from "@/app/components/ui/glare-card";
 
+type SorotanCardProps = {
+    title: string;
+    description: string;
+    image: string;
+    tagline?: string;
+}
+
+const SorotanCard = ({title, description, image, tagline}: SorotanCardProps) => {
+    return (
+        <div className="flex px-10 gap-10">
+            <GlareCard className="w-screen  ">
+                <img
+                    className="h-full w-full absolute inset-0 object-cover"
+                    src={image}
+                />
+            </GlareCard>
+            <div className="flex flex-col items-start justify-center max-w-xl py-8 px-6">
+                <p>{tagline}</p>
+                <h1 className="font-bold text-2xl">{title}</h1>
+                <p className="font-normal text-base mt-4">
+                    {description}
+                </p>
+            </div>
+        </div>
+    )
+}
+
 export default  function Sorotan() {
     return (
-        <div>
-            <h1 className="text-2xl font-semibold text-center">Sorotan</h1>
-            <div className="flex px-10 gap-10">
-                <GlareCard className="flex flex-col items-center justify-center w-1/3">
-                    <img
-                        className="h-full w-full absolute inset-0 object-cover"
-                        src="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    />
-                </GlareCard>
-
-                <div className="w-2/3 flex flex-col justify-center gap-4">
-                    <p className="text-blue-600">Yang Di Rawat Seperti Anak</p>
-                    <h1 className="text-2xl font-semibold">Perkebunan Jagung</h1>
-                    <p className="text-justify">
-                        Dengan Luas wilayah 812,5 HA Desa Dirgahayu memiliki lahan pertanian yang cukup besar yaitu
-                        hampir 1/4 wilayah desa. Dengan begitu sektor pertanian menjadi komoditas besar di desa ini,
-                        padi menjadi pilihan utama petani sebagai tanaman primer untuk di tanam dan di kelola. Dengan
-                        lahan pertanian yang luas sektor pertanianpun sangat berpengaruh sekali terhadap kestabilan
-                        ekonomi masyarakat dan Desa.
-                    </p>
-                </div>
+    <div className="flex flex-col gap-10 text-white  bg-gradient-to-b from-bg to-primary py-5 w-full items-center">
+            <div>
+                <h1 className="text-2xl font-semibold text-center">Sorotan</h1>
+                <p className="w-2/3 mx-auto text-center">
+                    Berikut adalah beberapa sorotan terkait desa dirgahayu yang bisa menjadi informasi tambahan:
+                </p>
             </div>
+            <SorotanCard
+                tagline="Yang Di Rawat Seperti Anak"
+                title="Sawah yang luas penghasil padi"
+                description="Banyaknya lahan perkebunan kelapa membawa berkah lain bagi masyarakat dan enokomi desa, yaitu produk unggulan desa berupa Gula Merah (Gula Kelapa) yang di buat dengan air nira kelapa. Dengan begitu banyak masyarakat yang mencari alternatif profesi menjadi penyadap Kelapa. Gula merah menjadi produk unggulan desa dengan nilai ekonomi yang tinggi dan stabil."
+                image="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <SorotanCard
+                tagline="Yang Di Rawat Seperti Anak"
+                title="Sawah yang luas penghasil padi"
+                description="Banyaknya lahan perkebunan kelapa membawa berkah lain bagi masyarakat dan enokomi desa, yaitu produk unggulan desa berupa Gula Merah (Gula Kelapa) yang di buat dengan air nira kelapa. Dengan begitu banyak masyarakat yang mencari alternatif profesi menjadi penyadap Kelapa. Gula merah menjadi produk unggulan desa dengan nilai ekonomi yang tinggi dan stabil."
+                image="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+            <SorotanCard
+                title="The greatest trick"
+                description="The greatest trick the devil ever pulled was to convince the world that he didn't"
+                image="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
         </div>
     )
 }
