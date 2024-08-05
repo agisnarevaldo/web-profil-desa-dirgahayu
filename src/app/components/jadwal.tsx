c4f26e35a48309fe7f38d4fee1028731fd500b20
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-05 19:01:53 +0700

    refactor: Update Jadwal component styling and structure

--- src/app/components/jadwal.tsx
+++ src/app/components/jadwal.tsx
@@ -5,17 +5,17 @@ type JadwalCardProps = {
 
 const Item = ({ hari, jam }: JadwalCardProps) => {
     return (
-        <div className="flex justify-between py-2 px-4 border border-black">
+        <div className="flex justify-between py-2 px-4 border border-t-0 border-x-0 border-black shadow-md shadow-slate-500">
             <p>{hari}</p>
             <p>{jam}</p>
         </div>
-    )
+    )       
 }
 
 export default function Jadwal() {
     return (
-        <div className="border rounded-xl border-black flex flex-col w-1/4 gap-2 py-2">
-            <div className="py-2">
+        <div className="border rounded-xl border-black flex flex-col w-1/4 py-2 shadow-slate-500 shadow-lg">
+            <div className="py-2 border-b border-black">
                 <h1 className="text-center font-semibold">Waktu dan Hari Kerja</h1>
             </div>
             <Item hari="Senin" jam="08:00 - 15:00"/>
