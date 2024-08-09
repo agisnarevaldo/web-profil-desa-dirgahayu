1988f603d9ab5513adccbce27f868d907d07024c
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-09 07:10:43 +0700

    feat: Add About page with HeroScrollDemo component

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import {HeroScrollDemo} from "@/app/components/HeroScroll";
+import Link from "next/link";
+
+export default function About () {
+    return (
+        <main>
+            <HeroScrollDemo/>
+        </main>
+    )
+}
