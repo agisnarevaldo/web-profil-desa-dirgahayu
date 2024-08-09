5417b7a85f1aadf14dac3bdc9998c5cb6bd66b92
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-09 07:31:46 +0700

    refactor: Remove unused imports and components from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 
-import Image from "next/image";
-import Navbar from "@/app/components/navbar";
-import {ImagesSliderDemo} from "@/app/components/hero";
 import Profile from "@/app/components/profile";
-import LayananBak from "@/app/components/layanan.bak";
-import Jadwal from "@/app/components/jadwal";
-import {GlareCard} from "@/app/components/ui/glare-card";
-import Layanan from "@/app/components/layanan";
-import Staff from "@/app/components/staff";
 import StaffSlide from "@/app/components/staffSlide";
 import Sorotan from "@/app/components/sorotan";
 import {ImagesSlidertest} from "@/app/components/hero1";
 import Footer from "@/app/components/footer";
 import ButtonTop from "@/app/components/btnTop";
-// import ImageSlider from "@/app/components/ui/slider";
 
 export default function Home() {
   return (
