86fae16302a58eeeb28bfdb53233de4009e2e19c
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-09 07:12:26 +0700

    refactor: Update Layanan component with responsive width for larger screens

--- src/app/components/layanan.tsx
+++ src/app/components/layanan.tsx
@@ -23,9 +23,9 @@ const LayananCard = ({title, description, icon}: LayananCardProps) => {
 
 export default function Layanan() {
     return (
-    <div className="w-2/4 flex flex-col gap-4">
+    <div className="lg:w-2/4 flex flex-col gap-4">
         <LayananCard title="PELAYANAN ADMINISTRATIF" description="Pendaftaran penduduk dan pencatatan sipil, Penerbitan surat-surat administratif, Pengelolaan data dan informasi desa." icon="medical-icon:i-administration"/>
-        <LayananCard title="PELAYANAN SOSIAL" description="Penyediaan bantuan s osial bagi penduduk yang membutuhkan (misalnya, PKH, sembako, dll), Program bantuan bagi lanjut usia, difabel, atau kelompok rentan lainnya." icon="medical-icon:i-social-services"/>
+        <LayananCard title="PELAYANAN SOSIAL" description="Penyediaan bantuan sosial bagi penduduk yang membutuhkan (misalnya, PKH, sembako, dll), Program bantuan bagi lanjut usia, difabel, atau kelompok rentan lainnya." icon="medical-icon:i-social-services"/>
         <LayananCard title="PELAYANAN EKONOMI" description="Pembinaan dan pengembangan usaha mikro, kecil, dan menengah di desa, Penyediaan pelatihan dan pendampingan untuk usaha produktif." icon="streamline:decent-work-and-economic-growth-solid"/>
         <LayananCard title="PELAYANAN KESEHATAN" description="Puskesmas atau posyandu untuk pelayanan kesehatan dasar, Program imunisasi dan kesehatan ibu dan anak, Pengelolaan dan distribusi obat-obatan dasar." icon="material-symbols:health-metrics"/>
     </div>
