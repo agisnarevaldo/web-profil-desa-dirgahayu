f2f1035cc21b41750eca5697aa7d80d38f508bd8
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-10 21:51:27 +0700

    refactor: Add VisiMisi and Footer components to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,16 @@
 import {HeroScrollDemo} from "@/app/components/HeroScroll";
 import Link from "next/link";
+import VisiMisi from "../components/visi-misi";
+import Footer from "@/app/components/footer";
 
 export default function About () {
     return (
         <main>
             <HeroScrollDemo/>
+
+            <VisiMisi />
+
+            <Footer />
         </main>
     )
 }
