b89a3decd0166ada257493592555be6453d24a33
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-10 21:22:58 +0700

    modify: Improve Navbar ul for mobile screen device

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -37,44 +37,44 @@ export default function Navbar() {
             </Link>
 
             <ul
-                className={`lg:flex lg:items-center lg:gap-6 lg:p-0 p-4 absolute lg:relative top-full left-0 w-full lg:w-auto bg-gray-100 bg-opacity-60 backdrop-blur-lg lg:bg-transparent shadow-lg lg:shadow-none transition-all duration-300 ease-in-out ${isMenuOpen ? 'block' : 'hidden'}`}
+                className={`text-center lg:backdrop-blur-none lg:bg-none lg:bg-transparent lg:flex lg:items-center lg:gap-6 lg:p-0 p-4 absolute lg:relative top-full left-0 w-full lg:w-auto bg-gray-100 bg-opacity-60 backdrop-blur-lg shadow-lg lg:shadow-none transition-all duration-300 ease-in-out ${isMenuOpen ? 'block' : 'hidden'}`}
                 role="menu"
             >
-                <li role="none">
+                <li role="none" className="py-2">
                     <Link
                         href="/" role="menuitem"
                         tabIndex={0}
-                        className="font-medium hover:text-fg"
+                        className="font-medium hover:text-fg flex justify-center"
                     >
                         Beranda
                     </Link>
                 </li>
-                <li role="none">
+                <li role="none" className="py-2">
                     <Link
                         href="/about"
                         role="menuitem"
                         tabIndex={0}
-                        className="font-medium hover:text-fg"
+                        className="font-medium hover:text-fg flex justify-center"
                     >
                         Tentang
                     </Link>
                 </li>
-                <li role="none">
+                <li role="none" className="py-2">
                     <Link
                         href="/pelayanan"
                         role="menuitem"
                         tabIndex={0}
-                        className="font-medium hover:text-fg"
+                        className="font-medium hover:text-fg flex justify-center"
                     >
                         Pelayanan
                     </Link>
                 </li>
-                <li role="none">
+                <li role="none" className="py-2">
                     <Link
                         href="/#struktur"
                         role="menuitem"
                         tabIndex={0}
-                        className="font-medium hover:text-fg"
+                        className="font-medium hover:text-fg flex justify-center"
                     >
                         Struktur
                     </Link>
