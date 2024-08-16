dba0a7106b9a7446f3bb08bace990e4913108661
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-17 03:49:17 +0700

    refactor: Add Lokasi component to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import {HeroScrollDemo} from "@/app/components/HeroScroll";
 import Link from "next/link";
 import VisiMisi from "../components/visi-misi";
 import Footer from "@/app/components/footer";
+import Lokasi from "@/app/components/lokasi";
 
 export default function About () {
     return (
@@ -9,6 +10,8 @@ export default function About () {
             <HeroScrollDemo/>
 
             <VisiMisi />
+
+            <Lokasi />
 
             <Footer />
         </main>
