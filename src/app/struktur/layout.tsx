dc5301e7ab07033c84e72056c1473016b568bdd0
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-20 20:03:44 +0700

    refactor: Update layout component with footer and sidebar changes

--- src/app/struktur/layout.tsx
+++ src/app/struktur/layout.tsx
@@ -1,10 +1,14 @@
 import Sidebar from "@/app/components/sidebar";
+import Footer from "@/app/components/footer";
 
 export default function StrukturLayout({children}: Readonly<{children: React.ReactNode;}>) {
     return (
-            <div className="flex flex-col lg:flex-row min-h-screen">
-                <Sidebar />
+        <div className="min-h-screen flex flex-col justify-between">
+            <div className="flex flex-col lg:flex-row ">
+                <Sidebar/>
                 {children}
             </div>
+            <Footer />
+        </div>
     )
 }
