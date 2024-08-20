c48acc46e84cf1ee07d1ccf98404b828c884c25e
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-20 20:02:34 +0700

    feat: Update profile image for Kepala Desa

--- src/app/struktur/kepala-desa/page.tsx
+++ src/app/struktur/kepala-desa/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <div className="flex flex-col items-center justify-start gap-10 pt-3 pl-10">
             <CardProfile
-                src="/kades.jpeg"
+                src="/kepdes.png    "
                 nama="ASEP BUDI WAHYUDIN"
                 jabatan="Kepala Desa"
                 pendidikan="SLTA"
