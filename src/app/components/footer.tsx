7d30eb3bff2beaf2193872cd28dca1bd641a2a85
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-22 00:14:34 +0700

    refactor: Update Footer component with copyright year and developer information

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,32 +1,33 @@
-import Link from "next/link";
+            import Link from "next/link";
 
-export default function Footer() {
-    const currentYear = new Date().getFullYear();
-    return (
-        <footer
-            className="bg-secondary border-t-[0.1px] border-t-slate-200 text-sm text-center pt-5 text-slate-100  w-full">
-            <div className="text-left px-12">
-                <h1 className="font-bold text-2xl">DESA DIRGAHAYU</h1>
-                <p>Jalan Paratag No. 01 Dirgahayu - Kadipaten - Tasikmalaya (46157)</p>
-            </div>
-            <div
-                className="flex justify-center items-center mt-4 py-2 bg-slate-100 w-full backdrop-blur-lg bg-opacity-30">
-                <p>Depeloved by{" "}
-                    <Link
-                        href="https://www.instagram.com/alfianrasyidin/"
-                        className="underline"
-                    >
-                        agisnarevaldo
-                    </Link>
-                    <span> @ </span>
-                    <Link
-                    href="https://www.instagram.com/kkn.dirgahayu2024/"
-                    className="underline"
-                >
-                    KKN UNPER
-                </Link> | </p>  
-                <p>&copy; {currentYear} Desa Dirgahayu</p>
-            </div>
-        </footer>
-    )
-}
+            export default function Footer() {
+                const currentYear = new Date().getFullYear();
+                return (
+                    <footer
+                        className="bg-secondary border-t-[0.1px] border-t-slate-200 text-sm text-center text-slate-100  w-full">
+                        {/*<div className="text-left px-12">*/}
+                        {/*    <h1 className="font-bold text-2xl">DESA DIRGAHAYU</h1>*/}
+                        {/*    <p>Jalan Paratag No. 01 Dirgahayu - Kadipaten - Tasikmalaya (46157)</p>*/}
+                        {/*</div>*/}
+                        <div
+                            className="py-2 bg-slate-100 w-full backdrop-blur-lg bg-opacity-10">
+                            <span>&copy;2024 Desa Dirgahayu{" "}</span>
+                            <p>{" "} Depeloved by{" "}
+                                <Link
+                                    href="https://www.instagram.com/alfianrasyidin/"
+                                    className="underline"           
+                                >
+                                    agisnarevaldo
+                                </Link>
+                                <span> at </span>
+                                <Link
+                                    href="https://www.instagram.com/kkn.dirgahayu2024/"
+                                    className="underline"
+                                >
+                                    KKN UNPER
+                                </Link></p>
+
+                        </div>
+                    </footer>
+                )
+            }
