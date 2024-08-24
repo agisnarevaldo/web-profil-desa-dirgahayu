0a89d2e8c6bee15d03fb6e71601fc29b2f935429
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-08-24 22:36:20 +0700

    refactor: Update sekdes page with new data and images

--- src/app/struktur/sekdes/page.tsx
+++ src/app/struktur/sekdes/page.tsx
@@ -3,51 +3,67 @@ import CardProfile from "@/app/components/cardProfile";
 const sekdesData = [
     {
         id: 1,
-        nama: "RINI SURYANI",
-        jabatan: "SEKRETARIS DESA",
-        pendidikan: "SLTA",
-        noSk: "-",
-        image: "/6.png"
-    },
-    {
-        id: 2,
         nama: "DEWI ROSMAWATI",
-        jabatan: "KAUR PERENCANAAN",
+        jabatan: "SEKRETARIS DESA",
         pendidikan: "S1",
         noSk: "-",
         image: "/7.png"
     },
     {
+        id: 2,
+        nama: "INA YASNI LESTARI",
+        jabatan: "KAUR UMUM",
+        pendidikan: "SLTA",
+        noSk: "-",
+        image: "/8.png"
+    },
+    {
         id: 3,
-        nama: "SENDHY BUDHY UTAMA",
+        nama: "IDA SITI SO'IDAH",
+        jabatan: "KAUR PERENCANAAN",
+        pendidikan: "SLTA",
+        noSk: "-",
+        image: "/11.png"
+    },
+    {
+        id: 4,
+        nama: "RINI SURYANI",
+        jabatan: "KAUR KEUANGAN",
+        pendidikan: "SLTA",
+        noSk: "-",
+        image: "/6.png"
+    },
+    {
+        id:5,
+        nama: "SANDHY BUDHY",
         jabatan: "KASI KESEJAHTERAAN",
         pendidikan: "SLTA",
         noSk: "-",
         image: "/10.png"
     },
     {
-        id: 4,
-        nama: "DANY SOMANTIKA",
-        jabatan: "KASI KEPEMERINTAHAN",
-        pendidikan: "SLTA",
-        noSk: "-",
-        image: "https://api.dicebear.com/9.x/initials/svg?seed=DS"
-    },
-    {
-        id: 5,
-        nama: "IDA SITI SOIDAH",
-        jabatan: "KAUR TU DAN UMUM",
-        pendidikan: "SLTA",
-        noSk: "-",
-        image: "/11.png"
-    },
-    {
         id: 6,
+        nama: "DANY SOMANTIKA",
+        jabatan: "KASI PEMERINTAHAN",
+        pendidikan: "SLTA",
+        noSk: "-",
+        image: "/5.png"
+    },
+    {
+        id: 7,
         nama: "DUDUNG",
         jabatan: "KASI PELAYANAN",
         pendidikan: "SLTA",
         noSk: "-",
         image: "/1.png"
+    },
+    {
+        id : 8,
+        nama : "ANAN ROSIDIN",
+        jabatan : "OPERATOR DESA",
+        pendidikan : "_",
+        noSk : "-",
+        image : "/12.png"
     },
 ]
 
