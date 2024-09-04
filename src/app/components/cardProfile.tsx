6f5520ea04c209b9c5e213a759c591dd2371ea53
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-05 00:45:37 +0700

    refactor: Replace img tag with Image component in cardProfile.tsx

--- src/app/components/cardProfile.tsx
+++ src/app/components/cardProfile.tsx
@@ -14,7 +14,7 @@ export default function CardProfile({src, nama, jabatan, pendidikan, noSk, class
         <div className={className}>
             <h1 className="text-2xl mb-2">{jabatan}</h1>
             <div className={`flex mx-auto items-center w-full gap-4`}>
-                <img src={src} alt="Photo" width={150} height={150} className="rounded-md"/>
+                <Image src={src} alt="Photo" width={150} height={150} className="rounded-md"/>
                 <table>
                     <tbody>
                     <tr>
